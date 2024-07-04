c4ed4514c7f4ebb351ec2b0fc81515f1941efbb0
pameladeysarkar2003 <153226113+pameladeysarkar2003@users.noreply.github.com>
2024-07-04 12:43:19 +0530

    Add files via upload

---
+++ generalstoremanagement.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+class temp{
+    string itemID,itemName;
+    int itemQuantity,itemPrice;
+    fstream file;
+    public:
+    void addProduct(void);
+    void viewProduct(void);
+    void buyProduct(void);
+}obj;
+
+int main(){
+    char choice;
+
+    cout<<"press 1"->"Start Shopping";
+    cout<<"press 0"->"Exit";
+    cin>>choice;
+    switch(choice)
+    {
+        case 'A':
+
+        break;
+        case 3:
+
+        break;
+    }
+}
+
+
+
